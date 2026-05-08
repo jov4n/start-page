ee43928b2fb3449f372eee1f3b39ab9b228f5fe2
jov4n <jovan.bundalo@gmail.com>
2026-05-08 14:54:04 -0600

    Add Nix flake environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Windows Start Page development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+
+          shellHook = ''
+            echo "Windows Start Page Dev Environment"
+            echo "Run 'npm install' to install dependencies"
+            echo "Run 'npm run dev' to start the development server"
+          '';
+        };
+      }
+    );
+}
